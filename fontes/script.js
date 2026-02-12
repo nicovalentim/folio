53d48a2e10b7ad7bc6ab89732c9fa32939d7734a
Nico Valentim <50474720+nicovalentim@users.noreply.github.com>
2026-02-12 14:08:07 -0300

    Add files via upload

--- fontes/script.js
+++ fontes/script.js
@@ -1,13 +1,45 @@
+// botão que aparece na página e executa a funça
 const botao = document.querySelector("#verMais");
+
+// lugar aonde os posts vão aparecer
 const posts = document.querySelector("#posts");
 
+// lista de posts em html separado pq preguiça refazer a página toda vez no futuro
+const arquivos = [
+	"post1.html",
+	"post2.html"
+];
+
+// variáveis/constantes pra função rodar
+let contador = 0;
 botao.addEventListener("click", carregar);
 
 async function carregar() {
-	const response = await fetch("posts.html");
+
+	// fazer o botão sumir caso n tenha mais arquivos pra carregar (pfv que funcione dessa vez)
+	if (contador >=arquivos.length) {
+		botao.style.display = "none";
+		return
+	}
+
+
+	botao.disabled = true
+	botao.textContent = "Carregando...";
+
+	// adicionar próximo arquivo
+	const response = await fetch("frontpage/posts.html");
 	const html = await response.text();
-	posts.innerHTML = html;
-	botao.style.display = "none";
-}
+	posts.innerHTML += html;
 
+	// subir o contador pro botão sumir
+	contador++
+	}
 
+	// maldito botão, some logo
+	if (contador >=arquivos.length) {
+		botao.style.display = "none";
+	}
+	else {
+		botao.disabled = false;
+		botao.textContent = "+";
+	}
